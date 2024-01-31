0226904a29cf50d5ae6b35f9a81bb73ae83426be
davidcoats <david@getsafetycone.com>
2024-01-31 11:47:26 -0800

    Initial commit.

---
+++ source/D8S.E0012/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace D8S.E0012
+{
+	/// <summary>
+	/// Rendering Razor components to a string in net8.0.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/D8S.E0012/Project Plan.md
@@ -0,0 +1,2 @@
+# D8S.E0012
+Rendering Razor components to a string in net8.0.
